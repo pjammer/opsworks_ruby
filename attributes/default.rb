bd8fe71226a67a72da82dd527ab73629fb0c8ba9
Philip Ingram <philip@ingraminternet.com>
2016-07-19 05:47:28 -0400

    deleting db setup with rds

--- attributes/default.rb
+++ attributes/default.rb
@@ -74,7 +74,7 @@ default['defaults']['framework']['adapter'] = 'rails'
 default['defaults']['framework']['migrate'] = true
 default['defaults']['framework']['migration_command'] =
   '/usr/local/bin/bundle exec rake db:version > /dev/null 2>&1 ' \
-  '&& /usr/local/bin/bundle exec rake db:migrate || /usr/local/bin/bundle exec rake db:setup'
+  '&& /usr/local/bin/bundle exec rake db:migrate'
 default['defaults']['framework']['assets_precompile'] = true
 default['defaults']['framework']['assets_precompilation_command'] = '/usr/local/bin/bundle exec rake assets:precompile'
 

05c0d575b863723546d178aed3d91c1fd61392d6
Philip Ingram <philip@ingraminternet.com>
2016-08-05 10:23:14 -0400

    hardcoding application.yml symlink

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -33,9 +33,8 @@ every_enabled_application do |application, deploy|
     purge_before_symlink(
       (node['defaults']['deploy']['purge_before_symlink'] + Array.wrap(deploy[:purge_before_symlink])).uniq
     )
-    Chef::Log.info "app environment is: #{application['environment']}"
-    Chef::Log.info "deploy environment is: #{framework.out[:deploy_environment]}"
-    #continue with original code
+    Chef::Log.info deploy[:symlink_before_migrate]
+    symlink_before_migrate {'config/application.yml' => 'config/application.yml'}
     symlink_before_migrate deploy[:symlink_before_migrate]
     symlinks(node['defaults']['deploy']['symlinks'].merge(deploy[:symlinks] || {}))
 

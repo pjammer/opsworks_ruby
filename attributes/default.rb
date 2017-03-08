e057bf966a6b4f01d47859e7bb43ad3050531a91
Philip Ingram <philip@ingraminternet.com>
2017-03-08 05:51:12 -0500

    updating for longer timeouts and 0 body size

--- attributes/default.rb
+++ attributes/default.rb
@@ -56,14 +56,14 @@ default['defaults']['webserver']['extra_config_ssl'] = ''
 
 default['nginx']['default_site_enabled'] = false
 default['defaults']['webserver']['build_type'] = 'default'
-default['nginx']['client_body_timeout'] = default['defaults']['webserver']['client_body_timeout'] = '60'
-default['nginx']['client_header_timeout'] = default['defaults']['webserver']['client_header_timeout'] = '60'
-default['nginx']['client_max_body_size'] = default['defaults']['webserver']['client_max_body_size'] = '400m'
-default['nginx']['keepalive_timeout'] = default['defaults']['webserver']['keepalive_timeout'] = '15'
+default['nginx']['client_body_timeout'] = default['defaults']['webserver']['client_body_timeout'] = '120'
+default['nginx']['client_header_timeout'] = default['defaults']['webserver']['client_header_timeout'] = '120'
+default['nginx']['client_max_body_size'] = default['defaults']['webserver']['client_max_body_size'] = '0'
+default['nginx']['keepalive_timeout'] = default['defaults']['webserver']['keepalive_timeout'] = '120'
 default['nginx']['log_dir'] = default['defaults']['webserver']['log_dir'] = '/var/log/nginx'
-default['nginx']['proxy_read_timeout'] = default['defaults']['webserver']['proxy_read_timeout'] = '60'
-default['nginx']['proxy_send_timeout'] = default['defaults']['webserver']['proxy_send_timeout'] = '60'
-default['nginx']['send_timeout'] = default['defaults']['webserver']['send_timeout'] = '60'
+default['nginx']['proxy_read_timeout'] = default['defaults']['webserver']['proxy_read_timeout'] = '120'
+default['nginx']['proxy_send_timeout'] = default['defaults']['webserver']['proxy_send_timeout'] = '120'
+default['nginx']['send_timeout'] = default['defaults']['webserver']['send_timeout'] = '120'
 
 # framework
 ## common
